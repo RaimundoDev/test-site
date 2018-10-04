a28314c14b696f5120ce13f0e1491d6c08286b35
Raimundo Nonato <raimundo.nonatotj18@outlook.com>
2018-10-04 16:16:02 -0300

    Main.js add, and functions

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/background') {
+      myImage.setAttribute ('src','images/background2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/background');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'The big weeb, ' + myName;
+  }
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'The big weeb: ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }
+
+alert('Hello your weeb!' + storedName);
